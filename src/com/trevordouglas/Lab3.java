a378baf0b3a51fbe3f9ac13b1082d75fa64dc6b2
Trevor Douglas <tdoug19@gmail.com>
2017-10-06 15:26:35 -0600

    InitialEclipseCommit

---
+++ src/com/trevordouglas/Lab3.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.trevordouglas;
+
+/**
+ * @author douglatr
+ *
+ */
+public class Lab3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
