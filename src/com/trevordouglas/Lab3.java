437233a254592baa9769fae7e902fcdded7b92cb
Trevor Douglas <tdoug19@gmail.com>
2017-10-25 14:45:32 -0600

    Added a new Print statement.

--- src/com/trevordouglas/Lab3.java
+++ src/com/trevordouglas/Lab3.java
@@ -15,6 +15,7 @@ public class Lab3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		System.out.println("This world is awesome.!!");
 
 	}
 
