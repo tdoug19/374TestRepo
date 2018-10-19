f8cf15504995d1aaa0b82a1d2325f3128da37f49
TD <tdoug19@gmail.com>
2018-10-19 13:00:42 -0600

    Added a class

---
+++ AnotherEclipseProject/src/test/ClassA.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author douglatr
+ *
+ */
+public class ClassA {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
